86ccb705e81e9220fbf0874653819a4082761306
lunatictiol <bhatsabiq9@gmail.com>
2024-04-20 15:33:11 +0530

    scafold basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,28 @@
 import Link from "next/link";
 
+const mockUrls=[
+  "https://utfs.io/f/cb04f8cb-06c2-4559-8713-0b3ef3d30776-eqyvam.jpg",
+  "https://utfs.io/f/5b4e0be8-8705-4647-8cef-5ae7ffcf776f-1518c7.jpg",
+  "https://utfs.io/f/d605e335-892e-4a19-9a72-2e780350cd13-1ff77d.png",
+  "https://utfs.io/f/acca1279-643c-476e-87f4-165468e2fe36-c7b85s.jpg"
+];
+const mockImages = mockUrls.map((url,index)=>({
+  id:index+1,
+ url  
+}))
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+     {
+      [...mockImages,...mockImages,...mockImages,...mockImages,...mockImages].map((image)=>(
+        <div key={image.id} className="w-48" >
+          <img src={image.url} alt="image" />
         </div>
+      ))
+     }
       </div>
+    
     </main>
   );
 }
